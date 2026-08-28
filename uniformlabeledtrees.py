014eb419a6e6bea1d842b0e88ca0b2da364cc0ff
emialvarezg <119767873+emialvarezg@users.noreply.github.com>
2026-08-28 17:34:58 -0600

    Update uniformlabeledtrees.py

--- uniformlabeledtrees.py
+++ uniformlabeledtrees.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 #para cosas de gráficas
-import networkx as nx
-import pydot
-from networkx.drawing.nx_pydot import graphviz_layout
+#import networkx as nx
+#import pydot
+#from networkx.drawing.nx_pydot import graphviz_layout
 
 print('prueba')
