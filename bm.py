ab6fd0c34aa7eefaf2be86c0ecfb73d2ee5a5cc3
emialvarezg <119767873+emialvarezg@users.noreply.github.com>
2026-08-28 20:15:06 -0600

    Create bm.py

---
+++ bm.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pyscript import display
+
+n=100000
+x=0
+b = [x]
+for i in range(n):
+    x+= np.sqrt(1/n)*np.random.normal()
+    b.append(x)
+t = [i/n for i in range(n+1)]
+
+fig, ax = plt.subplots()
+ax.plot(t,b , linewidth = 0.5, color= 'cornflowerblue')
+
+display(fig,target="bm")
