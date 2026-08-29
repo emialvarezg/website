d230ddc3ac8f9bac377c0a2b6a62834b72119adf
emialvarezg <119767873+emialvarezg@users.noreply.github.com>
2026-08-28 21:56:41 -0600

    Update uniformlabeledtrees.py

--- uniformlabeledtrees.py
+++ uniformlabeledtrees.py
@@ -34,7 +34,7 @@ def prufer(A):
 
 @when("click", "#submit-btn")
 def generate_tree(event):
-    document.getElementById("graf2").innerHTML = ""
+    document.getElementById('graf2').innerHTML = ""
     n = int(document.getElementById('num').value)
     S = [np.random.randint(1,n+1) for i in range(n-2)]
     G = prufer(S)
