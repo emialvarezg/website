ce6218ab22e8d59aed30b1858f878834c3d2522d
emialvarezg <119767873+emialvarezg@users.noreply.github.com>
2026-08-28 20:04:39 -0600

    Update uniformlabeledtrees.py

--- uniformlabeledtrees.py
+++ uniformlabeledtrees.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pyscript import display
 #para cosas de gráficas
-#import networkx as nx
+import networkx as nx
 #import pydot
 #from networkx.drawing.nx_pydot import graphviz_layout
 
@@ -14,3 +14,39 @@ fig, ax = plt.subplots()
 ax.plot(x,y)
 
 display(fig,target="graf")
+
+def prufer(A):
+    #definimos el árbol
+    T = nx.Graph()
+
+    #etiquetas disponibles
+    et = [i+1 for i in range(len(A)+2)]
+
+    #definimos los vértices
+    T.add_nodes_from(et)
+
+    #iteramos hasta que la secuencia esté vacía
+    while len(A) > 0:
+        #nos fijamos en las etiquetas restantes que no aparecen en la secuencia
+        disp = [j for j in et if j not in A]
+        #agregamos el primer elemento del la secuencia y la menor etiqueta disponible como arista
+        T.add_edge(A[0],min(disp))
+        #quitamos la etiqueta y la entrada de la secuencia que utilizamos
+        A.remove(A[0])
+        et.remove(min(disp))
+        
+    #finalmenta agregamos como arista las dos etiquetas restantes    
+    T.add_edge(et[0],et[1])
+    return T
+
+n=20
+S = [np.random.randint(1,n+1) for i in range(n-2)]
+G = prufer(S)
+
+fig2 = plt.figure()
+ax2 = plt.subplot()
+pos = nx.spring_layout(G)
+nx.draw(G, pos)
+display(fig2,target="graf2")
+
+
