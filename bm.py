254acbd7469efe0ad545ef9ec44ceb16a7f276ab
emialvarezg <119767873+emialvarezg@users.noreply.github.com>
2026-08-29 10:31:20 -0600

    Update bm.py

--- bm.py
+++ bm.py
@@ -14,7 +14,7 @@ def simular(event):
         b.append(x)
     t = [i/n for i in range(n+1)]
     
-    fig, ax = plt.subplots(figsize=(10, 6))
+    fig, ax = plt.subplots(figsize=(10, 7))
     ax.plot(t,b , linewidth = 0.5, color= 'cornflowerblue')
     
     display(fig,target="bm")
