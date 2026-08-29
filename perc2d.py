ded19e8d40a6efaa4a90baad1a623c9327f47e8c
emialvarezg <119767873+emialvarezg@users.noreply.github.com>
2026-08-29 09:55:24 -0600

    Create perc2d.py

---
+++ perc2d.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pyscript import display, web, when, document
+#para cosas de gráficas
+import networkx as nx
+
+def percolacion(n,p):
+    L=nx.grid_2d_graph(n,n)
+    for edge in L.edges():
+        if np.random.uniform()>p:
+            L.remove_edge(edge[0],edge[1])
+    return L
+
+
+@when("click", "#submit")
+def generate_perc(event):
+    plt.close()
+    document.getElementById("perc").innerHTML = ""
+    n = int(document.getElementById("num").value)
+    p = float(document.getElementById("prob").value)
+
+    L=percolacion(n,p)
+    pos = {node: node for node in L.nodes()}
+
+    fig, ax = plt.subplots()
+    nx.draw(L,pos,node_size=0,edge_color='darkviolet')
+    display(fig,target="perc")
