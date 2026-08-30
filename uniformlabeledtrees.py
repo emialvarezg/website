8cb31e91ce7e51837d36fc39ef8b98c0b4f5f0b6
emialvarezg <119767873+emialvarezg@users.noreply.github.com>
2026-08-29 20:07:20 -0600

    Update uniformlabeledtrees.py

--- uniformlabeledtrees.py
+++ uniformlabeledtrees.py
@@ -41,7 +41,7 @@ def generate_tree(event):
     G = prufer(S)
 
     fig2, ax2 = plt.subplots(figsize=(9, 9))
-    pos = nx.kamada_kawai_layout(G)
+    pos = nx.spring_layout(G)
     nx.draw(G, pos, node_color='forestgreen', node_size=150, with_labels=True, edge_color='saddlebrown',font_color='white',font_size='8')
     display(fig2,target="graf2")
 
